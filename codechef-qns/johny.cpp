f70a8910ea8d47a37feb4d4c3238f76b0dfb67e5
Yao Hong <yaohong6782@gmail.com>
2020-09-30 18:42:35 +0800

    code chef

---
+++ codechef-qns/johny.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,k,n;
+   
+    cin >> t;
+    while (t--){
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+        cin >> k; 
+        int temp = 0; //save the number 
+        for (int i = 0; i < n; i++){
+            if (i == k-1){
+                temp = arr[i];
+            }
+        }
+        int ans = 0;
+        sort(arr, arr+n);
+        for (int i = 0; i < n; i++){
+            if (arr[i] == temp){
+                ans = i;
+            }
+        }
+        cout << ans+1 << endl; 
+    }
+ }
+
+
